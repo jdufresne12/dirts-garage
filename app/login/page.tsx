6df7f97f5af7a772bac302da28cbf168315e605e
Jdufresne12 <jdufresne135@gmail.com>
2025-08-07 11:41:51 -0400

    commit

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -53,7 +53,7 @@ export default function LoginPage() {
     };
 
     return (
-        <div className="min-h-screen flex justify-center bg-gray-950 py-12 px-4 sm:px-6 lg:px-8">
+        <div className="min-h-screen flex items-center justify-center bg-gray-950 py-12 px-4 sm:px-6 lg:px-8">
             <div className="max-w-md w-full space-y-8">
                 <div>
                     <Image
